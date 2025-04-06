1e2bfc4e2de53d4021d820fee992a21c7ef83355
karthikprasadm <155703431+karthikprasadm2728@users.noreply.github.com>
2025-04-06 21:15:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
+import base64
 import os
 
 app = Flask(__name__)
 
+# Load the model
+model = tf.keras.models.load_model('model/mobilenetv3_sign_language_model.keras')
+
+# Classes (update based on your training)
+CLASSES = ['Hello', 'Yes', 'No', 'I Love You', 'Okay', 'Please', 'Thank You']
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    image_data = data['image']  # base64 image string
+
+    # Decode base64
+    image_bytes = base64.b64decode(image_data.split(',')[1])
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    image = image.resize((224, 224))  # Resize as per your model input
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    predictions = model.predict(img_array)
+    class_index = np.argmax(predictions[0])
+    result = CLASSES[class_index]
+
+    return {'prediction': result}
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
